fcc3fb838288819792943d8873a7964c964353a9
thebestknown <jeimyespinoza971@gmail.com>
2025-01-16 15:56:44 -0500

    order changed

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -14,8 +14,8 @@ class Program
             Console.WriteLine("1. Write a new entry");
             Console.WriteLine("2. Display all entries");
             Console.WriteLine("3. Display entries by tag");
-            Console.WriteLine("4. Save journal to a file");
-            Console.WriteLine("5. Load journal from a file");
+            Console.WriteLine("4. Load journal from a file");
+            Console.WriteLine("5. Save journal to a file");
             Console.WriteLine("6. Quit");
             Console.Write("Choose an option: ");
 
@@ -50,17 +50,17 @@ class Program
             }
             else if (choice == "4")
             {
-                // Save to a file
-                Console.Write("Enter filename to save to: ");
-                string fileName = Console.ReadLine();
-                theJournal.SaveToFile(fileName);
-            }
-            else if (choice == "5")
-            {
                 // Load from a file
                 Console.Write("Enter filename to load from: ");
                 string fileName = Console.ReadLine();
                 theJournal.LoadFromFile(fileName);
+            }
+             else if (choice == "5")
+            {
+                // Save to a file
+                Console.Write("Enter filename to save to: ");
+                string fileName = Console.ReadLine();
+                theJournal.SaveToFile(fileName);
             }
             else if (choice == "6")
             {
