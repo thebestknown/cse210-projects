0709c7f71633af810778e6f98a163612cd7e0831
thebestknown <jeimyespinoza971@gmail.com>
2025-01-21 19:06:57 -0500

    Defined fraction class with attributes and constructors

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,43 @@
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator)
+    {
+        _numerator = numerator;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public int GetNumerator()
+    {
+        return _numerator;
+    }
+
+    public void SetNumerator(int numerator)
+    {
+        _numerator = numerator;
+    }
+
+    public int GetDenominator()
+    {
+        return _denominator;
+    }
+
+    public void SetDenominator(int denominator)
+    {
+        _denominator = denominator;
+    }
+}
