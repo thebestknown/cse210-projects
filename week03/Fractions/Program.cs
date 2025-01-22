94aa521f075cfd107f9f1e7499fddadc06ad367c
thebestknown <jeimyespinoza971@gmail.com>
2025-01-21 20:10:29 -0500

    Program finished

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 4);
+
+        Console.WriteLine(f1.GetFractionString()); 
+        Console.WriteLine(f1.GetDecimalValue());   
+
+        Console.WriteLine(f2.GetFractionString()); 
+        Console.WriteLine(f2.GetDecimalValue());   
+
+        Console.WriteLine(f3.GetFractionString()); 
+        Console.WriteLine(f3.GetDecimalValue());   
+
+        // Test setters and getters
+        f3.SetNumerator(1);
+        f3.SetDenominator(3);
+
+        Console.WriteLine(f3.GetFractionString()); 
+        Console.WriteLine(f3.GetDecimalValue());   
     }
 }
+
