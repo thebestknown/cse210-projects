abf6a860821d559c535964b790f8d8b7a43f913d
thebestknown <jeimyespinoza971@gmail.com>
2025-01-28 16:34:30 -0500

    Created Comment class

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Comment
+{
+    private string _name;
+    private string _text;
+
+    public Comment(string name, string text)
+    {
+        _name = name;
+        _text = text;
+    }
+
+    public void DisplayComment()
+    {
+        Console.WriteLine($"{_name}: {_text}");
+    }
+}
