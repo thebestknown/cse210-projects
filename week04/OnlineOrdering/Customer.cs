d902761a6ff4888d7b48906b679163249e81bd69
thebestknown <jeimyespinoza971@gmail.com>
2025-01-29 15:19:10 -0500

    finished the customer class

---
+++ week04/OnlineOrdering/Customer.cs
@@ -0,0 +1,21 @@
+class Customer
+{
+    private string _name;
+    private Address _address;
+
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
+
+    public bool IsInUSA()
+    {
+        return _address.IsInUSA();
+    }
+
+    public string GetCustomerInfo()
+    {
+        return $"{_name}\n{_address.GetFullAddress()}";
+    }
+}
