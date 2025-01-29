d22236a928e92e2aef9bffa0d5ccc38c39371d1c
thebestknown <jeimyespinoza971@gmail.com>
2025-01-29 15:42:12 -0500

    Products class finished

---
+++ week04/OnlineOrdering/Product.cs
@@ -0,0 +1,25 @@
+class Product
+{
+    private string _name;
+    private int _productId;
+    private double _price;
+    private int _quantity;
+
+    public Product(string name, int productId, double price, int quantity)
+    {
+        _name = name;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
+
+    public double GetTotalCost()
+    {
+        return _price * _quantity;
+    }
+
+    public string GetProductInfo()
+    {
+        return $"Product ID: {_productId}, Name: {_name}";
+    }
+}
