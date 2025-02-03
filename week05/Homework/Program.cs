59ec8ffe15d0702bdcc290d8ddc777fed965bc2e
thebestknown <jeimyespinoza971@gmail.com>
2025-02-03 15:33:46 -0500

    Math assignment class done

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -6,5 +6,9 @@ class Program
     {
         Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
         Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
     }
 }
