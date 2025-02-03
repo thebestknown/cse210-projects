513e30354a013eefd8837e4d49d740159f8e2981
thebestknown <jeimyespinoza971@gmail.com>
2025-02-03 15:56:35 -0500

    Finished 3erd class

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -10,5 +10,9 @@ class Program
         MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
         Console.WriteLine(mathAssignment.GetSummary());
         Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
