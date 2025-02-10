37bf0d4cf804a888fa58276fecf52391f964b8ef
thebestknown <jeimyespinoza971@gmail.com>
2025-02-10 16:30:44 -0500

    Rectangle class finished

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("Red", 5.0);
+
+        Console.WriteLine($"Square Color: {square.GetColor()}");
+        Console.WriteLine($"Square Area: {square.GetArea()}");
     }
 }
