0696357d6d737526ab31106fae441574c6d46246
thebestknown <jeimyespinoza971@gmail.com>
2025-02-10 18:53:39 -0500

    Circle class finished and classes tested

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Square square = new Square("Red", 5.0);
+        Rectangle rectangle = new Rectangle("Blue", 4.0, 6.0);
+        Circle circle = new Circle("Green", 3.0);
 
-        Console.WriteLine($"Square Color: {square.GetColor()}");
-        Console.WriteLine($"Square Area: {square.GetArea()}");
+        Console.WriteLine($"Square Color: {square.GetColor()}, Area: {square.GetArea()}");
+        Console.WriteLine($"Rectangle Color: {rectangle.GetColor()}, Area: {rectangle.GetArea()}");
+        Console.WriteLine($"Circle Color: {circle.GetColor()}, Area: {circle.GetArea()}");
     }
 }
