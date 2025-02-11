c9fb11bbf7e56c0ea8a18e5c6297ae5f1d23e62c
thebestknown <jeimyespinoza971@gmail.com>
2025-02-10 19:24:06 -0500

    Program finished

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,16 +1,27 @@
 using System;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Square square = new Square("Red", 5.0);
-        Rectangle rectangle = new Rectangle("Blue", 4.0, 6.0);
-        Circle circle = new Circle("Green", 3.0);
+        List<Shape> shapes = new List<Shape>();
 
-        Console.WriteLine($"Square Color: {square.GetColor()}, Area: {square.GetArea()}");
-        Console.WriteLine($"Rectangle Color: {rectangle.GetColor()}, Area: {rectangle.GetArea()}");
-        Console.WriteLine($"Circle Color: {circle.GetColor()}, Area: {circle.GetArea()}");
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Blue", 4, 5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Green", 6);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
