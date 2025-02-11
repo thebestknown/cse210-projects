1c9c8f8c88156db2f4cd6413fdf83f368c8f4454
thebestknown <jeimyespinoza971@gmail.com>
2025-02-11 16:04:11 -0500

    Goal class finished

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,18 @@
+abstract class Goal
+{
+    protected string _shortName;
+    protected string _description;
+    public int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+    public abstract bool IsComplete();
+    public abstract string GetDetailsString();
+    public abstract string GetStringRepresentation();
+}
