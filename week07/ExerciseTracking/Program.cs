4280c1fe8b6e40832680aef1f6b85981b06d2583
thebestknown <jeimyespinoza971@gmail.com>
2025-02-17 17:26:25 -0500

    Final program excersice tracking

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running(new DateTime(2022, 11, 3), 30, 3.0));
+        activities.Add(new Cycling(new DateTime(2022, 11, 3), 30, 6.0));
+        activities.Add(new Swimming(new DateTime(2022, 11, 3), 30, 10));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
